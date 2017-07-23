3372a3fbadcf74ef10addeac6f36b127d82eb545
David Darke <david@atomicsmash.co.uk>
2017-07-23 13:29:05 +0100

    Added composer for download testing =

--- image-optimise.sh
+++ image-optimise.sh
@@ -1,18 +1,17 @@
+#!/bin/bash
+# echo ${0%/*}
+# cd ${0%/*}
+cd ../
+ls
 
 
-# https://regex101.com/r/iZ3fQ3/3
-
-
-
-
-
-for f in $(find * -name '*.jpg' -or -name '*.JPG'); do
-
-    # Dead simple filter to grab WP cropped image, it looks for
-    # [number][number][the letter x][number][number]
-    # This sequence appears at the end of WP cropped images like:
-    # 2016/09/structured-data-in-wordpress-392x250.jpg
-    if [[ $f =~ -[0-9][0-9]\x[0-9][0-9] ]] ; then
-        jpegoptim -m 80 $f
-    fi
-done
+# for f in $(find * -name '*.jpg' -or -name '*.JPG'); do
+#
+#     # Dead simple filter to grab WP cropped image, it looks for
+#     # [number][number][the letter x][number][number]
+#     # This sequence appears at the end of WP cropped images like:
+#     # 2016/09/structured-data-in-wordpress-392x250.jpg
+#     if [[ $f =~ -[0-9][0-9]\x[0-9][0-9] ]] ; then
+#         jpegoptim -m 80 $f
+#     fi
+# done
